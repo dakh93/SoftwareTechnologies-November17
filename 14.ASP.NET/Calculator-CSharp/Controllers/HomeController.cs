8f418f31eff5a055fbd3dea7cf37e84579d1d8e7
dakh93 <dakh93>
2017-12-13 12:17:35 +0200

    ASP.NET Calculator

---
+++ 14.ASP.NET/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web.Mvc;
+using Calculator_CSharp.Models;
+using Microsoft.Ajax.Utilities;
+
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.Result = CalculateResult(calculator);
+
+            return RedirectToAction("Index", calculator);
+        }
+
+        private decimal CalculateResult(Calculator calculator)
+        {
+            decimal result = 0M;
+            switch (calculator.Operator) 
+            {
+                case "+":
+                    result = calculator.LeftOperand + calculator.RightOperand;
+                    break;
+                case "-":
+                    result = calculator.LeftOperand - calculator.RightOperand;
+                    break;
+                case "*":
+                    result = calculator.LeftOperand * calculator.RightOperand;
+                    break;
+                case "/":
+                    result = calculator.LeftOperand / calculator.RightOperand;
+                    break;
+                case "%":
+                    result = calculator.LeftOperand % calculator.RightOperand;
+                    break;
+                case "^":
+                    result = 1M;
+                    for (int i = 0; i < calculator.RightOperand; i++)
+                    {
+                        result *= calculator.LeftOperand;
+                    }
+                    break;
+
+            }
+            return result;
+            
+        }
+    }
+}
