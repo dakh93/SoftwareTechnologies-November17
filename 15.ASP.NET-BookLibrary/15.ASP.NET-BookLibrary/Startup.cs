571feff6493c7fa49d6781ffb7b9e1b2de9d9084
dakh93 <dakh93>
2017-12-13 18:53:26 +0200

    ASP.NET BookLibrary

---
+++ 15.ASP.NET-BookLibrary/15.ASP.NET-BookLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_15.ASP.NET_BookLibrary.Startup))]
+namespace _15.ASP.NET_BookLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
